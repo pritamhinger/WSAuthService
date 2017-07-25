967253514de38ce41700cc83cb6023f13c903fa0
Pritam Hinger <pritamgenius24@gmail.com>
2017-07-25 11:45:33 +0530

    Formatted code. Add MVC Default Routes.

--- WSAuthService/Controllers/TenantUserController.cs
+++ WSAuthService/Controllers/TenantUserController.cs
@@ -9,48 +9,48 @@ namespace WSAuthService.ProjectController
 {
 	[Route("user")]
 	public class TenantUserController : Controller
-    {
+	{
 
-        [HttpPut("{id}/user/{userId}")]
-        public IActionResult UpdateUser(long id, long userId, [FromBody] User user)
-        {
-            if (user == null || user.Id != userId) {
-                return BadRequest();
-            }
+		[HttpPut("{id}/user/{userId}")]
+		public IActionResult UpdateUser(long id, long userId, [FromBody] User user)
+		{
+			if (user == null || user.Id != userId) {
+				return BadRequest();
+			}
 
-            var tenant = TestData.GetTenantById(id);
-            if (tenant == null) {
-                return BadRequest();
-            }
+			var tenant = TestData.GetTenantById(id);
+			if (tenant == null) {
+				return BadRequest();
+			}
 
-            var userRef = tenant.Users.Where(tempUser => tempUser.Id == userId).First<User>();
-            if (userRef == null) {
-                return BadRequest();
-            }
+			var userRef = tenant.Users.Where(tempUser => tempUser.Id == userId).First<User>();
+			if (userRef == null) {
+				return BadRequest();
+			}
 
-            // TODO:  Update User here
-            return new NoContentResult();
-        }
+			// TODO:  Update User here
+			return new NoContentResult();
+		}
 
-        [HttpDelete("{id}/user/{userId}")]
-        public IActionResult DeleteUser(long id, long userId, [FromBody] User user)
-        {
-            if (user == null || user.Id != userId) {
-                return BadRequest();
-            }
+		[HttpDelete("{id}/user/{userId}")]
+		public IActionResult DeleteUser(long id, long userId, [FromBody] User user)
+		{
+			if (user == null || user.Id != userId) {
+				return BadRequest();
+			}
 
-            var tenant = TestData.GetTenantById(id);
-            if (tenant == null) {
-                return BadRequest();
-            }
+			var tenant = TestData.GetTenantById(id);
+			if (tenant == null) {
+				return BadRequest();
+			}
 
-            var userRef = tenant.Users.Where(tempUser => tempUser.Id == userId).First<User>();
-            if (userRef == null) {
-                return BadRequest();
-            }
+			var userRef = tenant.Users.Where(tempUser => tempUser.Id == userId).First<User>();
+			if (userRef == null) {
+				return BadRequest();
+			}
 
-            // TODO:  Delete User here
-            return new NoContentResult();
-        }
-    }
+			// TODO:  Delete User here
+			return new NoContentResult();
+		}
+	}
 }
--- WSAuthService/Startup.cs
+++ WSAuthService/Startup.cs
@@ -18,98 +18,98 @@ using WSAuthService.Authentication;
 
 namespace WSAuthService
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-            services.AddIdentityServer()
-                .AddTemporarySigningCredential()
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryApiResources(Config.GetApiResource())
-                .AddInMemoryClients(Config.GetClients())
-                .AddTestUsers(Config.GetUsers());
-        }
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+			services.AddIdentityServer()
+				.AddTemporarySigningCredential()
+				.AddInMemoryIdentityResources(Config.GetIdentityResources())
+				.AddInMemoryApiResources(Config.GetApiResource())
+				.AddInMemoryClients(Config.GetClients())
+				.AddTestUsers(Config.GetUsers());
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            app.UseIdentityServer();
-            app.UseCookieAuthentication(new CookieAuthenticationOptions {
-                AuthenticationScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
-                AutomaticAuthenticate = false,
-                AutomaticChallenge = false
-            });
+			app.UseIdentityServer();
+			app.UseCookieAuthentication(new CookieAuthenticationOptions {
+				AuthenticationScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
+				AutomaticAuthenticate = false,
+				AutomaticChallenge = false
+			});
 
-            app.UseGoogleAuthentication(new GoogleOptions {
-                AuthenticationScheme = "Google",
-                DisplayName = "Login With Google",
-                SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
-                ClientId = "910648818463-vfuocte751i8ntmnsubbfaa7s31chsse.apps.googleusercontent.com",
-                ClientSecret = "lTlc-5UWoh_hXHe9PoX0akC_"
-            });
+			app.UseGoogleAuthentication(new GoogleOptions {
+				AuthenticationScheme = "Google",
+				DisplayName = "Login With Google",
+				SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
+				ClientId = "910648818463-vfuocte751i8ntmnsubbfaa7s31chsse.apps.googleusercontent.com",
+				ClientSecret = "lTlc-5UWoh_hXHe9PoX0akC_"
+			});
 
-            var schemeName = "AzureAD";
-            var dataProtectionProvider = app.ApplicationServices.GetRequiredService<IDataProtectionProvider>();
-            var distributedCache = app.ApplicationServices.GetRequiredService<IDistributedCache>();
+			var schemeName = "AzureAD";
+			var dataProtectionProvider = app.ApplicationServices.GetRequiredService<IDataProtectionProvider>();
+			var distributedCache = app.ApplicationServices.GetRequiredService<IDistributedCache>();
 
-            var dataProtector = dataProtectionProvider.CreateProtector(
-                typeof(OpenIdConnectMiddleware).FullName,
-                typeof(string).FullName, schemeName,
-                "v1");
+			var dataProtector = dataProtectionProvider.CreateProtector(
+				typeof(OpenIdConnectMiddleware).FullName,
+				typeof(string).FullName, schemeName,
+				"v1");
 
-            var dataFormat = new CachedPropertiesDataFormat(distributedCache, dataProtector);
+			var dataFormat = new CachedPropertiesDataFormat(distributedCache, dataProtector);
 
-            ///
-            /// Azure AD Configuration
-            /// 
-            
+			///
+			/// Azure AD Configuration
+			/// 
+			
 
-            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions {
-                AuthenticationScheme = schemeName,
-                DisplayName = "Pritam AzureAD",
-                SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
-                ClientId = "2beaf9f4-9eeb-4219-983e-ae8e454b70e6",
-                Authority = $"https://login.microsoftonline.com/common/",
-                ResponseType = OpenIdConnectResponseType.IdToken,
-                StateDataFormat = dataFormat,
-                TokenValidationParameters = new TokenValidationParameters {
-                    ValidateIssuer = false,
-                    AuthenticationType = "idsrv.external"
+			app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions {
+				AuthenticationScheme = schemeName,
+				DisplayName = "Pritam AzureAD",
+				SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
+				ClientId = "2beaf9f4-9eeb-4219-983e-ae8e454b70e6",
+				Authority = $"https://login.microsoftonline.com/common/",
+				ResponseType = OpenIdConnectResponseType.IdToken,
+				StateDataFormat = dataFormat,
+				TokenValidationParameters = new TokenValidationParameters {
+					ValidateIssuer = false,
+					AuthenticationType = "idsrv.external"
 
-                }
-            });
+				}
+			});
 
-            //app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions {
-            //    AuthenticationScheme = schemeName,
-            //    DisplayName = "Anil AzureAD",
-            //    SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
-            //    ClientId = "50b223a5-8242-4c57-91c6-ef446f9dbe08",
-            //    Authority = $"https://login.microsoftonline.com/ded39aca-1c7d-456a-bc06-5c8088ce1624",
-            //    ResponseType = OpenIdConnectResponseType.IdToken,
-            //    StateDataFormat = dataFormat
-            //});
+			//app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions {
+			//    AuthenticationScheme = schemeName,
+			//    DisplayName = "Anil AzureAD",
+			//    SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
+			//    ClientId = "50b223a5-8242-4c57-91c6-ef446f9dbe08",
+			//    Authority = $"https://login.microsoftonline.com/ded39aca-1c7d-456a-bc06-5c8088ce1624",
+			//    ResponseType = OpenIdConnectResponseType.IdToken,
+			//    StateDataFormat = dataFormat
+			//});
 
-            app.UseStaticFiles();
-			//app.UseMvcWithDefaultRoute();
-			//app.UseMvc(routes => {
-			//    routes.MapRoute(name: "tenant", template: "{controller=tenant}/{action=GetTenantForId}/{id?}");
-			// });
+			app.UseStaticFiles();
+			app.UseMvcWithDefaultRoute();
+			app.UseMvc(routes => {
+				routes.MapRoute(name: "tenant", template: "{controller=tenant}/{action=GetTenantForId}/{id?}");
+			});
 			app.UseMvc();
 
-            //app.Run(async (context) =>
-            //{
-            //    await context.Response.WriteAsync("Hello World!");
-            //});
-        }
-    }
+			//app.Run(async (context) =>
+			//{
+			//    await context.Response.WriteAsync("Hello World!");
+			//});
+		}
+	}
 }
